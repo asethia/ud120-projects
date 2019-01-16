8afa304066efe7e8b2b94c50b8fda1e266fbbea7
Arun Sethia <arun.sethia@metrosystems-external.net>
2019-01-15 18:18:52 -0600

    decision tree test

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,19 +13,25 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
+minSamplesSplit=40
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(selectPercentile=10)
 
 
-
+print("number of features in training data:",len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+clf=tree.DecisionTreeClassifier(min_samples_split=minSamplesSplit)
+clf=clf.fit(features_train, labels_train)
+
+acc=clf.score(features_test,labels_test)
 
 #########################################################
 
+print("accuracy:",acc)
 
