b7ebfe2fef1464de37650cc8095132ed3dd024ae
Arun Sethia <arun.sethia@metrosystems-external.net>
2019-01-15 18:27:36 -0600

    added for selectPercentile=1 and selectPercentile=10

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -17,21 +17,34 @@ minSamplesSplit=40
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess(selectPercentile=10)
+features_train_1, features_test_1, labels_train_1, labels_test_1 = preprocess(selectPercentile=1)
+
+#when selectPercentile=10
+features_train_10, features_test_10, labels_train_10, labels_test_10 = preprocess(selectPercentile=10)
 
 
-print("number of features in training data:",len(features_train[0]))
+print("number of features in training data when selectPercentile=1:",len(features_train_1[0]))
+
+print("number of features in training data when selectPercentile=10:",len(features_train_10[0]))
+
 
 #########################################################
 ### your code goes here ###
 
 from sklearn import tree
-clf=tree.DecisionTreeClassifier(min_samples_split=minSamplesSplit)
-clf=clf.fit(features_train, labels_train)
+clf_1=tree.DecisionTreeClassifier(min_samples_split=minSamplesSplit)
+clf_1=clf_1.fit(features_train_1, labels_train_1)
 
-acc=clf.score(features_test,labels_test)
+acc1=clf_1.score(features_test_1,labels_test_1)
+
+clf_10=tree.DecisionTreeClassifier(min_samples_split=minSamplesSplit)
+clf_10=clf_10.fit(features_train_10, labels_train_10)
+
+acc10=clf_10.score(features_test_10,labels_test_10)
 
 #########################################################
 
-print("accuracy:",acc)
+print("accuracy when selectPercentile=1:",acc1)
+
+print("accuracy when selectPercentile=10:",acc10)
 
